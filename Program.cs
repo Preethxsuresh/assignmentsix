a93794bb12995364f255080e35555cc024fcb203
Preethxsuresh <preethi.suresh@solitontech.com>
2023-08-27 23:46:51 +0530

    feat: Implementing syntax functionality for the greeting app.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace simple_app
+﻿namespace Simple_app
 {
-    internal class Program
+    /// <summary>
+    /// Program class carries out the task of Greeting User.
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Main method prompts user to enter name and greet the user.
+        /// </summary>
+        /// <param name="args"> Argument passed to the Greeting Application</param>
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Hello There !! \nEnter Your Name : ");
+            string? userName = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"\nHey {userName}, Welcome :)");
+
         }
     }
 }
